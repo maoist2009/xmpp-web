0a600df00af8c8da7c59631f749ab0e688783ebf
maoist2009 <maoist2009@outlook.com>
2025-03-10 20:35:08 +0800

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -132,7 +132,7 @@ export default {
     const jid = localStorage.getItem('jid')
     if (jid) {
       this.credentials.jid = jid
-      onJidBlur()
+      this.onJidBlur()
     }
     const password = localStorage.getItem('p')
     if (password) {
